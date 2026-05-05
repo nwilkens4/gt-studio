8d0b5cb4fb18f4e72c35e6ddc4d4b88bff67ccae
Noah Wilkens <noah@autohub.io>
2026-05-04 20:07:37 -0400

    feat: scaffold vite + react + tailwind + r3f project

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="w-full h-full flex">
+      <div className="flex-1">{ /* CarViewer goes here */ }</div>
+      <div className="w-80">{ /* Panel goes here */ }</div>
+    </div>
+  )
+}
